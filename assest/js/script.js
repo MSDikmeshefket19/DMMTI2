f28c81fd8dd7acb8a3cedd86de9a213bf7164473
Mertin <MSDikmeshefket19@codingburgas.bg>
2020-12-03 12:45:38 +0200

    Added function comments

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,28 +1,32 @@
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
+    // the array of the winning combinations
 const WINNING_COMBINATIONS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
+
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    // Accesing the html elements
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
 const winningMessageElement = document.getElementById('winningMessage')
 const restartButton = document.getElementById('restartButton')
 const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
 let circleTurn
-
+    // The start of the game
 startGame()
-
+    // Checks if the restat button is pressed, so the game can be restarted
 restartButton.addEventListener('click', startGame)
 
 function startGame() {
     circleTurn = false
+        // Iterates through every cell and cleans the playing space
     cellElements.forEach(cell => {
         cell.classList.remove(X_CLASS)
         cell.classList.remove(CIRCLE_CLASS)
@@ -34,19 +38,23 @@ function startGame() {
 }
 
 function handleClick(e) {
+    // Selects the cell we want to click 
     const cell = e.target
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+        //Draws the symbol
     placeMark(cell, currentClass)
+        // Checks if the game is over (win, loss, tie)
     if (checkWin(currentClass)) {
         endGame(false)
     } else if (isDraw()) {
         endGame(true)
     } else {
+        //If the game is not over it swaps the turns
         swapTurns()
         setBoardHoverClass()
     }
 }
-
+//Ends the game and displays result
 function endGame(draw) {
     if (draw) {
         winningMessageTextElement.innerText = 'Равни'
@@ -55,25 +63,28 @@ function endGame(draw) {
     }
     winningMessageElement.classList.add('show')
 }
+//Checks if the game is even
 
 function isDraw() {
     return [...cellElements].every(cell => {
         return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
     })
 }
-
+//Places the mark based of the argument which is passed through the function
 function placeMark(cell, currentClass) {
     cell.classList.add(currentClass)
 }
-
+//Swaps from from O TO X and the opposite
 function swapTurns() {
     circleTurn = !circleTurn
 }
 
 function setBoardHoverClass() {
+    // Removes the previous hover figure
     board.classList.remove(X_CLASS)
     board.classList.remove(CIRCLE_CLASS)
     if (circleTurn) {
+        // Checks if the program has to put O or X based on a variable that we used to indicate them
         board.classList.add(CIRCLE_CLASS)
     } else {
         board.classList.add(X_CLASS)
@@ -81,6 +92,7 @@ function setBoardHoverClass() {
 }
 
 function checkWin(currentClass) {
+    // Iterates through the winnin combinations and checks if the player has won the game.
     return WINNING_COMBINATIONS.some(combination => {
         return combination.every(index => {
             return cellElements[index].classList.contains(currentClass)
